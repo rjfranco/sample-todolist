2d332a5c5da41ccc72b4c2238f5e04b5f788b1e2
Ramiro Jr. Franco <rjfranco@gmail.com>
2014-03-10 17:52:37 -0700

    Initial Commit

---
+++ ToDoList/TDLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TDLAppDelegate.h
+//  ToDoList
+//
+//  Created by Ramiro Franco on 3/10/14.
+//  Copyright (c) 2014 Ramiro Franco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Ramiro Franco on 3/10/14.
+//  Copyright (c) 2014 Ramiro Franco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TDLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TDLAppDelegate class]));
+    }
+}
